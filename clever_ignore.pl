fd9fd43b06c18d5efa21d96af3ba32df8b8bfc76
notk0 <none>
2014-07-05 15:27:23 +0200

    Skeleton script

---
+++ clever_ignore.pl
@@ -0,0 +1,12 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use vars qw($VERSION %IRSSI);
+use Irssi;
+
+$VERSION = '0.1';
+%IRSSI = (
+	authors 	=> 'Radditz',
+	name 		=> 'clever_ignore',
+	description 	=> 'Ignore IRC users in a clever way',
+);
