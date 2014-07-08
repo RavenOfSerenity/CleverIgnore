291c9f202457afdbe8a5ea07579588acbb8ed917
notk0 <none>
2014-07-08 22:13:09 +0200

    Such noob, forgot to replace <year> <owner> in license template

--- clever_ignore.pl
+++ clever_ignore.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl
-#Copyright (c) <YEAR>, <OWNER>
+#Copyright (c) <2014>, <RavenOfSerenity@gmail.com>
 #All rights reserved.
 #
 #Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
